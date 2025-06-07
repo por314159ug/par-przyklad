04dfb87b341959723b4c210d7a84f02818ed01db
unknown <KIE_Admin@WZR-B23-WYK.it.ug>
2025-06-07 10:55:00 +0200

    Add swagger

--- Program.cs
+++ Program.cs
@@ -11,11 +11,10 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
